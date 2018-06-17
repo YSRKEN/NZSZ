3528f6b650d63129b9c190b61adde03a097c4376
YSRKEN <ysr.ken@gmail.com>
2018-06-17 10:27:53 +0900

    カレンダーの表を動的に生成するようにした

--- nzsz/src/app/calendar/calendar.component.ts
+++ nzsz/src/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
+  CalendarTable: DayInfo[][] = [];
 
   constructor() { }
 
   ngOnInit() {
+    // カレンダーのマス数だけ、情報を揃える
+    for(var y = 0; y < 5; ++y){
+      const weekLine: DayInfo[] = [];
+      for(var x = 0; x < 7; ++x){
+        weekLine.push(new DayInfo(0));
+      }
+      this.CalendarTable.push(weekLine);
+    }
   }
 
+}
+
+class DayInfo{
+  // 日付
+  value: number;
+  // 「今日」を表すか？
+  todayFlg: boolean = false;
+  // 「今月」の日付か？
+  thisMonthFlg: boolean = true;
+  // コンストラクタ
+  constructor(value: number){
+    this.value = value;
+  }
 }
