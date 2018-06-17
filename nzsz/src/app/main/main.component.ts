0af2d54462bf0eb0d48d96575a79a78f68cd7223
YSRKEN <ysr.ken@gmail.com>
2018-06-17 11:01:10 +0900

    「公式」および「ライブ」ボタンを押した際の挙動を設定した

--- nzsz/src/app/main/main.component.ts
+++ nzsz/src/app/main/main.component.ts
@@ -19,7 +19,16 @@ export class MainComponent implements OnInit {
     this.LiveInfoList = liveInfoList;
   }
 
+  /** 今日の日付を返す */
   get Today(): Date { 
     return new Date();
+  }
+
+  /** にじさんじの公式サイト(Twitter垢)のページを開く */
+  jumpNzSzSite(){
+    window.open('https://twitter.com/nijisanji_app');
+  }
+  jumpNzSzLive(){
+    window.open('https://www.youtube.com/channel/UCX7YkU9nEeaoZbkVLVajcMg/videos');
   }
 }
