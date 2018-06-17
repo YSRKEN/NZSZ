a1bf3dde06a6529552a581e6ed7ca642334fcd3c
YSRKEN <ysr.ken@gmail.com>
2018-06-17 17:24:19 +0900

    字数制限のルーチンに改良を加えた

--- nzsz/src/app/limit-str.pipe.ts
+++ nzsz/src/app/limit-str.pipe.ts
@@ -1,11 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+function bytes(x: string) {
+  return(encodeURIComponent(x).replace(/%../g,"x").length);
+}
+
 @Pipe({
   name: 'limitStr'
 })
 export class LimitStrPipe implements PipeTransform {
   transform(value: string, length: number): string {
-    if(value.length > length){
+    if(bytes(value) > length){
       return value.substr(0, length - 3) + "...";
     }else{
       return value;
